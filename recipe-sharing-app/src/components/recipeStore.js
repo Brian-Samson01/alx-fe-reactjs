a1e26eb1e28dc74ab96f7ea99a22dfc2a7251cdb
Brian-Samson01 <brian.s.mwangi01@gmail.com>
2025-03-24 23:41:03 +0300

    corrction

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,23 +2,16 @@ import { create } from 'zustand';
 
 const useRecipeStore = create((set) => ({
   recipes: [],
-
   addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
-
-  updateRecipe: (updatedRecipe) =>
-    set((state) => ({
-      recipes: state.recipes.map((recipe) =>
-        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
-      ),
-    })),
-
-  deleteRecipe: (id) =>
-    set((state) => ({
-      recipes: state.recipes.filter((recipe) => recipe.id !== id),
-    })),
+  deleteRecipe: (recipeId) => set((state) => ({
+    recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
+  })),
+  updateRecipe: (updatedRecipe) => set((state) => ({
+    recipes: state.recipes.map((recipe) =>
+      recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+    ),
+  })),
+  setRecipes: (recipes) => set({ recipes }), // This line ensures "setRecipes" exists
 }));
 
 export default useRecipeStore;
-  
-  //We have a  recipes  array that stores all the recipes. We have three methods to manipulate the recipes:  addRecipe ,  updateRecipe , and  deleteRecipe . 
-  // We can now use this store in our components.
